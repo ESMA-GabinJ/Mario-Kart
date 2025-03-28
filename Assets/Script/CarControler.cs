2bdd8dcf6cb531d51e8b8c80f47bce1f6ea431a5
ESMA-GabinJ <g.jourdan@rns.ecolescreatives.com>
2025-03-28 16:33:40 +0100

    final rendu

--- Assets/Script/CarControler.cs
+++ Assets/Script/CarControler.cs
@@ -1,32 +1,74 @@
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
+using TMPro; // Nécessaire pour le texte du compte à rebours
 
 public class CarControler : MonoBehaviour
 {
+    [SerializeField] private string _hAxisInputName = "Horizontal", _accelerateInputName = "Accelerate";
+    [SerializeField] private LayerMask _layerMask;
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private ParticleSystem _driftParticles;
+    [SerializeField] private TextMeshProUGUI _countdownText; // Texte pour le compte à rebours
 
-    [SerializeField]
-    private string _hAxisInputName = "Horizontal", _accelerateInputName = "Accelerate";
-    [SerializeField]
-    private LayerMask _layerMask;
-    [SerializeField]
-    private Rigidbody _rb;
-
-    private float _speed, _accelerationLerpInterpolator, _rotationInput; 
-    [SerializeField]
-    private float _speedMaxBasic = 3, _speedMaxTurbo = 20, _accelerationFactor, _rotationSpeed = 0.5f, _maxAngle =360;
-    private bool _isAccelerating, _isBoosting;
+    private float _speed, _accelerationLerpInterpolator, _rotationInput;
+    [SerializeField] private float _speedMaxBasic = 30, _speedMaxTurbo = 40, _accelerationFactor, _rotationSpeed = 100f, _maxAngle = 360;
+    private bool _isAccelerating, _isBoosting, _isDrifting, _raceStarted, _boostReady;
     private float _groundSpeedVariator;
+    private float _speedModifier = 1f; // Ajouté pour gérer le multiplicateur de vitesse
 
-    [SerializeField]
-    public Transform _carColliderAndMesh;
-    [SerializeField]
-    private AnimationCurve _accelerationCurve;
+    [SerializeField] public Transform _carColliderAndMesh;
+    [SerializeField] private AnimationCurve _accelerationCurve;
 
+    private float _driftCharge;
+    private int _driftDirection;
+    private string _driftKey;
+    private ParticleSystem.MainModule _driftParticleSettings;
 
-    public void Boost() //permet de boost si y a pas de boost en cours
+    private void Start()
+    {
+        _driftKey = gameObject.name == "P1" ? "f" : "right ctrl";
+
+        if (_driftParticles != null)
+        {
+            _driftParticleSettings = _driftParticles.main;
+            _driftParticles.Stop();
+        }
+
+        StartCoroutine(StartCountdown());
+    }
+
+    private IEnumerator StartCountdown()
+    {
+        int countdown = 3;
+        _countdownText.gameObject.SetActive(true);
+        _boostReady = false;
+        _raceStarted = false;
+
+        while (countdown > 0)
+        {
+            _countdownText.text = countdown.ToString();
+
+            // Lorsque le compte à rebours atteint 1, vérifie si le joueur garde la touche d'accélérer
+            if (countdown == 1)
+            {
+                if (Input.GetButton(_accelerateInputName)) // Vérifie si la touche d'accélérer est maintenue
+                {
+                    _boostReady = true;  // Le boost sera prêt
+                }
+            }
+
+            yield return new WaitForSeconds(1);
+            countdown--;
+        }
+
+        _countdownText.text = "GO!";
+        _raceStarted = true;
+
+        yield return new WaitForSeconds(0.5f);
+        _countdownText.gameObject.SetActive(false);
+    }
+
+    public void Boost()
     {
         if (!_isBoosting)
         {
@@ -40,9 +82,10 @@ public class CarControler : MonoBehaviour
         yield return new WaitForSeconds(3);
         _isBoosting = false;
     }
+
     void Update()
     {
-
+        if (!_raceStarted) return;
 
         _rotationInput = Input.GetAxis(_hAxisInputName);
 
@@ -50,83 +93,128 @@ public class CarControler : MonoBehaviour
         {
             _isAccelerating = true;
         }
-        if(Input.GetButtonUp(_accelerateInputName))
+        if (Input.GetButtonUp(_accelerateInputName))
         {
             _isAccelerating = false;
         }
 
-        if (Physics.Raycast(transform.position, transform.up * -1, out var info, 1, _layerMask))
+        if (Input.GetKeyDown(_driftKey))
+        {
+            StartDrift();
+        }
+        if (Input.GetKeyUp(_driftKey))
+        {
+            EndDrift();
+        }
+
+        if (Physics.Raycast(transform.position, -transform.up, out var info, 1, _layerMask))
         {
             Ground groundBellow = info.transform.GetComponent<Ground>();
-            if (groundBellow != null)
-            {
-                _groundSpeedVariator = groundBellow.speedVariator;
-            }
-            else
-            {
-                _groundSpeedVariator = 1;
-            }
+            _groundSpeedVariator = groundBellow != null ? groundBellow.speedVariator : 1;
         }
         else
         {
             _groundSpeedVariator = 1;
         }
+
+        // Le boost peut être activé si le joueur garde la touche d'accélérer après que le compte à rebours soit à 1
+        if (_boostReady && Input.GetButton(_accelerateInputName)) // Vérifie si la touche d'accélérer est toujours maintenue
+        {
+            Boost();
+            _boostReady = false;  // Le boost est activé une seule fois
+        }
     }
 
-    private bool IsOnSlope(out RaycastHit hit, out float angle, out float angleZ)
+    private void StartDrift()
     {
-        hit = new RaycastHit();
-        angle = 0f;
-        angleZ = 0f;
-        if(Physics.Raycast(transform.position+transform.forward*.5f,Vector3.down,out hit, 0.8f))
+        if (!_isDrifting && Mathf.Abs(_rotationInput) > 0.1f)
         {
-            angle = Vector3.Angle(Vector3.up, hit.normal);
-            angleZ = Vector3.Angle(Vector3.right, hit.normal);
-            return angle !=  0 && angle<=_maxAngle;
+            _isDrifting = true;
+            _driftDirection = _rotationInput > 0 ? 1 : -1;
+            _driftCharge = 0;
+
+            if (_driftParticles != null)
+            {
+                _driftParticles.Play();
+            }
         }
-        return false;
+    }
+
+    private void EndDrift()
+    {
+        if (_isDrifting)
+        {
+            _isDrifting = false;
+            if (_driftCharge >= 1)
+            {
+                Boost();
+            }
+
+            if (_driftParticles != null)
+            {
+                _driftParticles.Stop();
+            }
+        }
     }
 
     private void FixedUpdate()
     {
-        
+        if (!_raceStarted) return;
+
         if (_isAccelerating)
         {
             _accelerationLerpInterpolator += _accelerationFactor;
         }
         else
         {
-            _accelerationLerpInterpolator -= _accelerationFactor*2;
+            _accelerationLerpInterpolator -= _accelerationFactor * 2;
         }
 
         _accelerationLerpInterpolator = Mathf.Clamp01(_accelerationLerpInterpolator);
-        
 
-        if(_isBoosting)
+        if (_isBoosting)
         {
             _speed = _speedMaxTurbo;
         }
         else
         {
-            _speed = _accelerationCurve.Evaluate(_accelerationLerpInterpolator)*_speedMaxBasic*_groundSpeedVariator;
+            _speed = _accelerationCurve.Evaluate(_accelerationLerpInterpolator) * _speedMaxBasic * _groundSpeedVariator * _speedModifier;
         }
 
         var forward = transform.forward;
-        var angle = 0f;
-        var angleZ = 0f;
-        if(IsOnSlope(out var hit, out angle, out angleZ))
-        {
-            forward = Vector3.ProjectOnPlane(forward, hit.normal).normalized;
 
+        if (_isDrifting)
+        {
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + _rotationSpeed * Time.deltaTime * _rotationInput * 1.5f, 0);
+            _driftCharge = Mathf.Clamp(_driftCharge + Time.deltaTime, 0, 1.5f);
+
+            if (_driftParticles != null)
+            {
+                if (_driftCharge < 0.5f)
+                    _driftParticleSettings.startColor = Color.blue;
+                else if (_driftCharge < 1f)
+                    _driftParticleSettings.startColor = Color.yellow;
+                else
+                    _driftParticleSettings.startColor = Color.red;
+            }
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + _rotationSpeed * Time.deltaTime * _rotationInput, 0);
         }
 
-        _carColliderAndMesh.eulerAngles =  new Vector3(-angle,_carColliderAndMesh.eulerAngles.y, angleZ);
-        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + _rotationSpeed * Time.deltaTime * _rotationInput, 0);
-        _rb.MovePosition(transform.position+forward*_speed*Time.fixedDeltaTime);
+        _rb.MovePosition(transform.position + forward * _speed * Time.fixedDeltaTime);
     }
-    private void OnTriggerEnter(Collider other) //quand on touche le boost accelere
+
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("BoostPad"))
             Boost();
+    }
+
+    // Méthode pour modifier la vitesse du joueur (en fonction de la taille)
+    public void SetSpeedModifier(float modifier)
+    {
+        _speedModifier = modifier;
     }
 }
--- Assets/Script/ItemLighting.cs
+++ Assets/Script/ItemLighting.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "ItemLightning", menuName = "Scriptable Objects/ItemLightning")]
@@ -5,9 +6,37 @@ public class ItemLightning : Item
 {
     private GameObject _player;
 
-    public override void Activation(PlayerItemManager player) //Item qui réduit la taille
+    public override void Activation(PlayerItemManager player) // Item qui réduit la taille du joueur adverse
     {
-        _player = GameObject.FindGameObjectWithTag("Player");
-        _player.transform.localScale = _player.transform.localScale * 0.5f;
+        string targetPlayerName = player.gameObject.name == "P1" ? "P2" : "P1";
+        GameObject targetPlayer = GameObject.Find(targetPlayerName);
+
+        if (targetPlayer != null)
+        {
+            // Réduit la taille du joueur
+            targetPlayer.transform.localScale *= 0.5f;
+
+            // Divise la vitesse du joueur par deux
+            CarControler carController = targetPlayer.GetComponent<CarControler>();
+            if (carController != null)
+            {
+                carController.SetSpeedModifier(0.5f); // Divise la vitesse par 2
+            }
+
+            // Lance une coroutine pour restaurer la vitesse après 3 secondes
+            targetPlayer.GetComponent<MonoBehaviour>().StartCoroutine(RevertSpeed(targetPlayer, carController));
+        }
+    }
+
+    private IEnumerator RevertSpeed(GameObject targetPlayer, CarControler carController)
+    {
+        // Attend 3 secondes avant de remettre la vitesse à la normale
+        yield return new WaitForSeconds(3f);
+
+        // Restaure la vitesse à la normale
+        if (carController != null)
+        {
+            carController.SetSpeedModifier(1f); // Restaure la vitesse normale
+        }
     }
 }
